1e1ca6ef333866a5e680d071987608afd3cd8dc2
Austin Kaylor <AustinJKaylor@eaton.com>
2024-01-29 21:35:44 -0500

    Creating a book entity

---
+++ Hexagonal Architectural Pattern/Core/Entities/Book.cs
@@ -0,0 +1,13 @@
+﻿namespace Core.Entities;
+
+/// <summary>
+/// Represents a book in Library's Catalog
+/// </summary>
+public class Book
+{
+    public long Id { get; set; } 
+    public string Title { get; set; }
+    public string Author { get; set; }
+    public Guid ISBN { get; set; }
+    public float Price { get; set; }
+}
