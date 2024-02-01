dc98ff1290be218421f7dc3e2b2648604f7f56f1
Austin Kaylor <AustinJKaylor@eaton.com>
2024-01-31 20:45:45 -0500

    Adding unit tests for all 3 controllers

---
+++ Uri Versioning/API.UnitTests/WeatherController_V3_Tests.cs
@@ -0,0 +1,22 @@
+﻿using API.Controllers.V3;
+using FluentAssertions;
+using Microsoft.Extensions.Logging;
+using Moq;
+
+namespace API.UnitTests;
+
+public class WeatherControllerV3Tests
+{
+    private readonly Mock<ILogger<WeatherForecastController>> _loggerMock = new();
+
+    [Fact]
+    public void WhenGetEndpointCalled_ShouldReturnFiftyWeatherForecasts()
+    {
+        // Arrange 
+        var controller = new WeatherForecastController(_loggerMock.Object);
+        // Act
+        var result = controller.Get();
+        // Assert
+        result.Should().HaveCount(50);
+    }
+}
