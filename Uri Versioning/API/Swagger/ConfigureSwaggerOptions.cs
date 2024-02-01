7669606ce1b5cdc8fb80b3ca75d8d5e4ac5b3e24
Austin Kaylor <AustinJKaylor@eaton.com>
2024-01-31 20:07:11 -0500

    Adding some swagger specific files to make the Uri versioning magic work

---
+++ Uri Versioning/API/Swagger/ConfigureSwaggerOptions.cs
@@ -0,0 +1,37 @@
+﻿using Asp.Versioning.ApiExplorer;
+using Microsoft.Extensions.Options;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace API.Swagger;
+
+/// <summary>
+/// Enables Swashbuckle to understand and manage the API versions within the application
+/// </summary>
+public class ConfigureSwaggerOptions(IApiVersionDescriptionProvider provider) : IConfigureOptions<SwaggerGenOptions>
+{
+    public void Configure(SwaggerGenOptions options)
+    {
+        foreach (var description in provider.ApiVersionDescriptions)
+        {
+            options.SwaggerDoc(description.GroupName, CreateInfoForApiVersion(description));
+        }
+    }
+
+    private static OpenApiInfo CreateInfoForApiVersion(ApiVersionDescription description)
+    {
+        var info = new OpenApiInfo
+        {
+            Title = "Versioning Web API",
+            Version = description.ApiVersion.ToString(),
+            Description = "Description for the Versioning Web API"
+        };
+
+        if (description.IsDeprecated)
+        {
+            info.Description += " This API version has been deprecated.";
+        }
+
+        return info;
+    }
+}
