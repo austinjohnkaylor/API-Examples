6052c82241b2e84f793a7520102ebd892e5ebc05
Austin Kaylor <AustinJKaylor@eaton.com>
2024-02-01 21:43:43 -0500

    Adding all the code for the Model Validation

---
+++ Model State Validation/API/Controllers/PeopleController.cs
@@ -0,0 +1,35 @@
+﻿using API.ActionFilters;
+using API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class PeopleController(ILogger<PeopleController> logger) : ControllerBase
+{
+    private readonly ILogger<PeopleController> _logger = logger;
+
+    [HttpGet("{id:guid}")]
+    public IActionResult Get(Guid id)
+    {
+        _logger.LogInformation($"Getting person with Id {id}");
+        var person = new Person
+        {
+            Id = id,
+            FirstName = "Jane",
+            LastName = "Doe",
+            Age = 30,
+            Gender = Gender.Female
+        };
+        return Ok(person);
+    }
+
+    [HttpPost]
+    [ServiceFilter(typeof(ValidationFilterAttribute))]
+    public IActionResult Post([FromBody] Person person)
+    {
+        _logger.LogInformation($"Creating a new person");
+        return CreatedAtAction(nameof(Post), new { id = person.Id }, person);
+    }
+}
--- Model State Validation/API/Program.cs
+++ Model State Validation/API/Program.cs
@@ -1,6 +1,31 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+using API.ActionFilters;
+using Microsoft.AspNetCore.Mvc;
 
-app.MapGet("/", () => "Hello World!");
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+
+builder.Services.AddScoped<ValidationFilterAttribute>();
+
+builder.Services.Configure<ApiBehaviorOptions>(options
+    => options.SuppressModelStateInvalidFilter = true);
+
+builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddSwaggerGen();
+
+WebApplication app = builder.Build();
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.Run();
+
+// Added for integration tests
+public partial class Program;
