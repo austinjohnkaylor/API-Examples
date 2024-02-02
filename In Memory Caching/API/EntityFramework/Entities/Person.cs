008960d6eee72f4babbd80ee5c79367b4a0a1def
Austin Kaylor <AustinJKaylor@eaton.com>
2024-02-02 18:55:38 -0500

    Deleting all the old entities and adding a person entity

---
+++ In Memory Caching/API/EntityFramework/Entities/Person.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace InMemoryCaching.API.EntityFramework.Entities;
+
+/// <summary>
+/// Represents a person
+/// </summary>
+public class Person
+{
+    [Required] 
+    public Guid Id { get; set; }
+    [Required]
+    [MaxLength(50, ErrorMessage = "If your name is longer than 50 characters, you may want to consider a nickname")]
+    public string? FirstName { get; set; }
+    public string? MiddleName { get; set; }
+    [Required]
+    public string? LastName { get; set; }
+    [Range(1,130, ErrorMessage = "No human has ever lived longer than 130 years!")]
+    public int Age { get; set; }
+    public Gender Gender { get; set; }
+}
+
+public enum Gender
+{
+    Male,
+    Female
+}
