a80f59a10eec41275fccf0293dff88fa277ab04c
Austin Kaylor <AustinJKaylor@eaton.com>
2024-02-06 20:58:26 -0500

    Got 2 tests

--- In Memory Caching/API.UnitTests/PersonControllerTests.cs
+++ In Memory Caching/API.UnitTests/PersonControllerTests.cs
@@ -2,6 +2,7 @@
 using InMemoryCaching.API.Controllers;
 using InMemoryCaching.API.EntityFramework;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Caching.Memory;
 using Microsoft.Extensions.Logging;
 using Moq;
@@ -13,40 +14,61 @@ public class PersonControllerTests
 {
     private readonly Mock<PersonContext> _mockDbContext;
     private readonly Mock<ILogger<PersonController>> _mockLogger;
-    private readonly Mock<IMemoryCache> _mockMemoryCache;
     private readonly PersonController _personController;
+    private List<Person> _people;
+    private const string cacheKey = "ListOfPeople";
+    private MemoryCache _cache;
+
 
     public PersonControllerTests()
     {
         _mockDbContext = new Mock<PersonContext>();
         _mockLogger = new Mock<ILogger<PersonController>>();
-        _mockMemoryCache = new Mock<IMemoryCache>();
-        _personController = new PersonController(_mockDbContext.Object, _mockMemoryCache.Object, _mockLogger.Object);
+        _cache = new MemoryCache(new MemoryCacheOptions());
+        _personController = new PersonController(_mockDbContext.Object, _cache, _mockLogger.Object);
+        PersonGenerator.Generate(100);
+        _people = PersonGenerator.People;
     }
-
+    
+    /// <summary>
+    /// Tests when the cache contains data
+    /// </summary>
+    /// <returns>All the people from the cache instead of the database</returns>
     [Fact]
-    public async Task WhenPeopleExistInCache_ThenReturnOkResult()
+    public async Task GetPeople_ReturnsOkResult_WhenCacheContainsData()
     {
         // Arrange
-        const string cacheKey = "PeopleList";
-        // https://github.com/MichalJankowskii/Moq.EntityFrameworkCore
-        PersonGenerator.Generate(100);
-        IList<Person> people = PersonGenerator.People;
-        _mockDbContext.Setup(x => x.People).ReturnsDbSet(people);
-        
-        MemoryCacheEntryOptions cacheEntryOptions = new MemoryCacheEntryOptions().SetAbsoluteExpiration(TimeSpan.FromMinutes(1));
-        var cacheEntry = new Mock<ICacheEntry>();
-        _mockMemoryCache.Setup(m => m.CreateEntry(It.IsAny<object>())).Returns(cacheEntry.Object);
-        
+        _mockDbContext.Setup(x => x.People).ReturnsDbSet(new List<Person>());
+        _cache.Set(cacheKey, _people);
         // Act
         var result = await _personController.GetPeople();
         OkObjectResult? resultHttp = result.Result as OkObjectResult;
-        var resultPeople = resultHttp?.Value as List<Person>;
-        
+        var returnedPeople = resultHttp?.Value as List<Person>;
+
         // Assert
         resultHttp.Should().BeOfType<OkObjectResult>();
         resultHttp?.StatusCode.Should().Be(200);
-        resultPeople?.Count.Should().Be(people.Count);
+        returnedPeople.Should().Equal(_people);
+    }
+    
+    /// <summary>
+    /// Tests when the cache is empty 
+    /// </summary>
+    /// <returns>All the people from the database instead of the cache</returns>
+    [Fact]
+    public async Task GetPeople_FetchesFromDatabase_WhenCacheIsEmpty()
+    {
+        // Arrange
+        _mockDbContext.Setup(x => x.People).ReturnsDbSet(_people);
+        // Act
+        var result = await _personController.GetPeople();
+        OkObjectResult? resultHttp = result.Result as OkObjectResult;
+        var returnedPeople = resultHttp?.Value as List<Person>;
+
+        // Assert
+        resultHttp.Should().BeOfType<OkObjectResult>();
+        resultHttp?.StatusCode.Should().Be(200);
+        returnedPeople.Should().Equal(_people);
     }
     
 }
