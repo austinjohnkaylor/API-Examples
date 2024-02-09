6c94b7c2ee62961ae81a62786abc2c6f2be217f5
Austin Kaylor <AustinJKaylor@eaton.com>
2024-02-09 08:53:38 -0500

    Committing what I have right now

--- Open API Documentation/MinimalAPI.Versioning/Program.cs
+++ Open API Documentation/MinimalAPI.Versioning/Program.cs
@@ -1,11 +1,54 @@
-var builder = WebApplication.CreateBuilder(args);
+using Asp.Versioning;
+using Asp.Versioning.Builder;
+using Asp.Versioning.Conventions;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using MinimalAPI.Versioning;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+IServiceCollection services = builder.Services;
 
 // Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+services.AddEndpointsApiExplorer();
+services.AddApiVersioning(
+        options =>
+        {
+            // reporting api versions will return the headers
+            // "api-supported-versions" and "api-deprecated-versions"
+            options.ReportApiVersions = true;
 
-var app = builder.Build();
+            options.Policies.Sunset( 0.9 )
+                .Effective( DateTimeOffset.Now.AddDays( 60 ) )
+                .Link( "policy.html" )
+                .Title( "Versioning Policy" )
+                .Type( "text/html" );
+        } )
+    .AddApiExplorer(
+        options =>
+        {
+            // add the versioned api explorer, which also adds IApiVersionDescriptionProvider service
+            // note: the specified format code will format the version as "'v'major[.minor][-status]"
+            options.GroupNameFormat = "'v'VVV";
+
+            // note: this option is only necessary when versioning by url segment. the SubstitutionFormat
+            // can also be used to control the format of the API version in route templates
+            options.SubstituteApiVersionInUrl = true;
+        } );
+services.AddTransient<IConfigureOptions<SwaggerGenOptions>, ConfigureSwaggerOptions>();
+services.AddSwaggerGen( options => options.OperationFilter<SwaggerDefaultValues>() );
+
+WebApplication app = builder.Build();
+
+// Have to define the API version set in order to use it on endpoints
+ApiVersionSet versionSet = app.NewApiVersionSet()
+    .HasApiVersion(1.0)
+    .HasApiVersion(2.0)
+    .HasApiVersion(3.0)
+    .ReportApiVersions()
+    .Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -16,29 +59,17 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+#region Endpoints
 
-app.MapGet("/weatherforecast", () =>
-    {
-        var forecast = Enumerable.Range(1, 5).Select(index =>
-                new WeatherForecast
-                (
-                    DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                    Random.Shared.Next(-20, 55),
-                    summaries[Random.Shared.Next(summaries.Length)]
-                ))
-            .ToArray();
-        return forecast;
-    })
-    .WithName("GetWeatherForecast")
-    .WithOpenApi();
+app.MapGet("/", () => "Hello World!")
+    .WithName("Hello")
+    .WithDescription("Says hello.")
+    .Produces<string>()
+    .WithTags("Greeting")
+    .WithApiVersionSet(versionSet)
+    .HasApiVersion(1.0);
+
+
+#endregion
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
