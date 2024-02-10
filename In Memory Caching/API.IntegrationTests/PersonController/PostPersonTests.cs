f8c5057f38dc045d12406c0717b3acc20f334d13
Austin Kaylor <AustinJKaylor@eaton.com>
2024-02-09 19:07:24 -0500

    Adding a todo

--- In Memory Caching/API.IntegrationTests/PersonController/PostPersonTests.cs
+++ In Memory Caching/API.IntegrationTests/PersonController/PostPersonTests.cs
@@ -5,6 +5,10 @@ using Microsoft.AspNetCore.Mvc.Testing;
 
 namespace InMemoryCaching.API.IntegrationTests.PersonController;
 
+/// <summary>
+/// Consider doing to the following to implement segregation of test classes: https://learn.microsoft.com/en-us/ef/core/testing/testing-with-the-database
+/// </summary>
+/// <param name="factory"></param>
 [Collection("Sequential")]
 public class PostPersonTests(WebApplicationFactory<Program> factory) : IClassFixture<WebApplicationFactory<Program>>
 {
