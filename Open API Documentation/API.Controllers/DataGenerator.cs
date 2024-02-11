b8b4c7c1219c43fa91b00221f7b729e7a86313a4
Austin Kaylor <AustinJKaylor@eaton.com>
2024-02-11 14:43:15 -0500

    Finishing off the database seeder

--- Open API Documentation/API.Controllers/DataGenerator.cs
+++ Open API Documentation/API.Controllers/DataGenerator.cs
@@ -1,4 +1,5 @@
 ﻿using API.Controllers.EntityFramework;
+using Bogus;
 
 namespace API.Controllers;
 
@@ -7,12 +8,75 @@ namespace API.Controllers;
 /// </summary>
 public class DataGenerator
 {
+    private static int _customerId = 1;
+    public static List<Customer>? Customers { get; set; }
+    private static int _orderId = 1;
+    public static List<Order>? Orders { get; set; }
+    private static int _productId = 1;
+    public static List<Product>? Products { get; set; }
+
+    private static Faker? _f;
+    
     public static void Generate(int numberOfCustomersToGenerate, int numberOfOrdersPerCustomerToGenerate, int numberOfProductsPerOrderToGenerate)
     {
-        throw new NotImplementedException();
-    }
+        _f = new Faker();
+        Customers = [];
+        Orders = [];
+        Products = [];
 
-    public static List<Customer> Customers { get; set; }
-    public static List<Order> Orders { get; set; }
-    public static List<Product> Products { get; set; }
+        for (var i = 0; i < numberOfCustomersToGenerate; i++)
+        {
+            Customer customer = new()
+            {
+                Id = _customerId++,
+                FirstName = _f.Person.FirstName,
+                LastName = _f.Person.LastName,
+                Email = _f.Person.Email,
+                PhoneNumber = _f.Person.Phone,
+                Address = new Address
+                {
+                    Street = _f.Address.StreetAddress(),
+                    City = _f.Address.City(),
+                    State = _f.Address.State(),
+                    ZipCode = _f.Address.ZipCode()
+                }
+            };
+            Customers.Add(customer);
+
+            for (var j = 0; j < numberOfOrdersPerCustomerToGenerate; j++)
+            {
+                Order order = new()
+                {
+                    Id = _orderId++,
+                    Name = _f.Commerce.ProductName(),
+                    Description = _f.Commerce.ProductAdjective(),
+                    Price = _f.Random.Decimal(1, 1000),
+                    ShippingAddress = new Address
+                    {
+                        Street = _f.Address.StreetAddress(),
+                        City = _f.Address.City(),
+                        State = _f.Address.State(),
+                        ZipCode = _f.Address.ZipCode()
+                    },
+                    Status = _f.PickRandomParam("Pending", "Shipped", "Delivered"),
+                    TrackingNumber = _f.Random.AlphaNumeric(10)
+                };
+                customer.Orders.Add(order);
+                Orders.Add(order);
+
+                for (var k = 0; k < numberOfProductsPerOrderToGenerate; k++)
+                {
+                    Product product = new()
+                    {
+                        Id = _productId++,
+                        Name = _f.Commerce.ProductName(),
+                        Description = _f.Commerce.ProductAdjective(),
+                        Price = _f.Random.Decimal(1, 1000)
+                    };
+                    order.Products.Add(product);
+                    Products.Add(product);
+                }
+            }
+        }
+    }
 }
--- Open API Documentation/API.Controllers/DatabaseInitializer.cs
+++ Open API Documentation/API.Controllers/DatabaseInitializer.cs
@@ -5,12 +5,12 @@ namespace API.Controllers;
 public class DatabaseInitializer
 {
     /// <summary>
-    /// Initializes the People database and makes sure it has data in it
+    /// Initializes the Simple Store database and makes sure it has data in it
     /// </summary>
     /// <param name="dbContext">The <see cref="EntityFramework.SimpleStoreDbContext"/> Entity Framework DB Context</param>
-    /// <param name="numberOfCustomersToGenerate"></param>
-    /// <param name="numberOfOrdersPerCustomerToGenerate"></param>
-    /// <param name="numberOfProductsPerOrderToGenerate"></param>
+    /// <param name="numberOfCustomersToGenerate">The number of <see cref="Customer"/> records to generate</param>
+    /// <param name="numberOfOrdersPerCustomerToGenerate">The number of <see cref="Order"/> records to generate per <see cref="Customer"/></param>
+    /// <param name="numberOfProductsPerOrderToGenerate">The number of <see cref="Product"/> reocrds to generate per <see cref="Order"/></param>
     internal static void Initialize(SimpleStoreDbContext dbContext, int numberOfCustomersToGenerate, int numberOfOrdersPerCustomerToGenerate, int numberOfProductsPerOrderToGenerate)
     {
         ArgumentNullException.ThrowIfNull(dbContext, nameof(dbContext));
@@ -22,7 +22,9 @@ public class DatabaseInitializer
             
         DataGenerator.Generate(numberOfCustomersToGenerate, numberOfOrdersPerCustomerToGenerate, numberOfProductsPerOrderToGenerate);
 
-        dbContext.Customers.AddRange(DataGenerator.Customers);
+        dbContext.Customers.AddRange(DataGenerator.Customers ?? []);
+        dbContext.Orders.AddRange(DataGenerator.Orders ?? []);
+        dbContext.Products.AddRange(DataGenerator.Products ?? []);
 
         dbContext.SaveChanges();
     }
