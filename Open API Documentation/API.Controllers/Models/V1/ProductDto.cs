898b7e49d9d8fa8849464a0350e9707da11a9dfd
Austin Kaylor <AustinJKaylor@eaton.com>
2024-02-12 19:53:46 -0500

    Doing the same for the Product entity and the ProductDto

--- Open API Documentation/API.Controllers/Models/V1/ProductDto.cs
+++ Open API Documentation/API.Controllers/Models/V1/ProductDto.cs
@@ -8,30 +8,4 @@ public class ProductDto
     public string Description { get; set; }
     public decimal Price { get; set; }
     public string Category { get; set; }
-    public static ProductDto CreateInstance()
-    {
-        return new ProductDto();
-    }
-    
-    public static ProductDto CreateInstance(string name, string description, decimal price, string category)
-    {
-        return new ProductDto
-        {
-            Name = name,
-            Description = description,
-            Price = price,
-            Category = category
-        };
-    }
-
-    public static explicit operator ProductDto(Product product)
-    {
-        return new ProductDto
-        {
-            Name = product.Name,
-            Description = product.Description,
-            Price = product.Price,
-            Category = product.Category
-        };
-    }
 }
