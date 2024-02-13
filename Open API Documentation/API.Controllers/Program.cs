baea601b5baa025eb09447e46aa8956d80287524
Austin Kaylor <AustinJKaylor@eaton.com>
2024-02-12 23:24:03 -0500

    Lots of swagger changes. Also adding Redoc

--- Open API Documentation/API.Controllers/Program.cs
+++ Open API Documentation/API.Controllers/Program.cs
@@ -6,6 +6,7 @@ using Swashbuckle.AspNetCore.SwaggerGen;
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
+builder.Services.AddOpenApiDocument();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(options =>
@@ -24,14 +25,23 @@ builder.Services.AddScoped<DatabaseInitializer>();
 WebApplication app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseStaticFiles(); // necessary to customize the way swagger looks
 app.UseSwagger();
-app.UseSwaggerUI();
+app.UseSwaggerUI(options =>
+{
+    options.InjectStylesheet("/swagger-ui/custom.css"); // necessary to customize the way swagger looks
+});
+// Add ReDoc UI to interact with the document
+// Available at: http://localhost:<port>/redoc
+app.UseReDoc(options =>
+{
+    options.Path = "/redoc"; // serve the redoc UI at the /redoc endpoint https://localhost:<port>/redoc
+});
 app.SeedDatabase(1000,5, 5);
 
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
