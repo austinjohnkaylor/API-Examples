093b6cb6a4d4bdefec0d338c37d299853a4273e1
Austin Kaylor <AustinJKaylor@eaton.com>
2024-02-19 18:59:20 -0500

    Adding the old way to handle exception handling

---
+++ Error Handling/OldWay.GlobalExceptionHandlingMiddleware/GlobalExceptionHandler.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OldWay.GlobalExceptionHandlingMiddleware;
+
+public class GlobalExceptionHandler(
+    RequestDelegate next,
+    ILogger<GlobalExceptionHandler> logger)
+{
+    /// <summary>
+    /// a <see cref="RequestDelegate"/> that is used to invoke the next middleware in the pipeline
+    /// </summary>
+    /// <param name="context"></param>
+    public async Task InvokeAsync(HttpContext context)
+    {
+        try
+        {
+            await next(context);
+        }
+        catch (Exception exception)
+        {
+            logger.LogError(
+                exception, "Exception occurred: {Message}", exception.Message);
+
+            ProblemDetails problemDetails = new()
+            {
+                Status = StatusCodes.Status500InternalServerError,
+                Title = "Server Error"
+            };
+
+            context.Response.StatusCode =
+                StatusCodes.Status500InternalServerError;
+
+            await context.Response.WriteAsJsonAsync(problemDetails);
+        }
+    }
+}
