b659f4bb34b47550e6eeae16175b445d6f8f28ea
Austin Kaylor <AustinJKaylor@eaton.com>
2024-06-07 14:56:41 -0400

    Adding a file for the IdentityDatabaseContext.cs EFCore DbContext

---
+++ School System using ASPNetCore Identity/IdentityAPI/EntityFramework/IdentityDatabaseContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace IdentityAPI.EntityFramework;
+
+public class IdentityDatabaseContext : IdentityDbContext<IdentityUser>
+{
+    public IdentityDatabaseContext(DbContextOptions<IdentityDbContext> options) :
+        base(options)
+    { }
+}
