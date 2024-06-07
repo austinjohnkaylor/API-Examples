e98de3d83d5dc627f0f7cc9a54574d4a043c5cf6
Austin Kaylor <AustinJKaylor@eaton.com>
2024-06-07 16:58:44 -0400

    Overriding the default AspNetCoreUsers table in the IdentityDatabaseContext.cs with "Users"

--- School System using ASPNetCore Identity/Identity.API/IdentityDatabaseContext.cs
+++ School System using ASPNetCore Identity/Identity.API/IdentityDatabaseContext.cs
@@ -1,7 +1,22 @@
-﻿namespace Identity.API;
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Identity.API;
 
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 public class IdentityDatabaseContext(DbContextOptions<IdentityDatabaseContext> options)
-    : IdentityDbContext<SchoolSystemUser>(options);
+    : IdentityDbContext<SchoolSystemUser>(options)
+{
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        
+        modelBuilder.Entity<SchoolSystemUser>(entity =>
+        {
+            entity.ToTable("Users");
+        });
+    }
+}
+    
+   
