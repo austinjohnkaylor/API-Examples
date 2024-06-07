e6fa6ce7a0100fefbc195a2d553de665fac19c72
Austin Kaylor <AustinJKaylor@eaton.com>
2024-06-07 16:39:48 -0400

    Adding an enum for GradeLevel.cs

---
+++ School System using ASPNetCore Identity/Identity.API/GradeLevel.cs
@@ -0,0 +1,18 @@
+﻿namespace Identity.API;
+
+public enum GradeLevel
+{
+    Kindergarten,
+    FirstGrade,
+    SecondGrade,
+    ThirdGrade,
+    FourthGrade,
+    FifthGrade,
+    SixthGrade,
+    SeventhGrade,
+    EighthGrade,
+    NinthGrade,
+    TenthGrade,
+    EleventhGrade,
+    TwelfthGrade
+}
