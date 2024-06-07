97d67864ce4250b3ac717148743c4240414de7af
Austin Kaylor <AustinJKaylor@eaton.com>
2024-06-07 16:03:28 -0400

    Creating an EF Core DbContext class for the Identity Database

---
+++ School System using ASPNetCore Identity/Identity.API/IdentityDatabaseContext.cs
@@ -0,0 +1,8 @@
+﻿namespace Identity.API;
+
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+public class IdentityDatabaseContext(DbContextOptions<IdentityDatabaseContext> options)
+    : IdentityDbContext<IdentityUser>(options);
