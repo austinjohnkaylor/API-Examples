5759937e5fdcb278a541da0706c014f025c410ce
Austin Kaylor <AustinJKaylor@eaton.com>
2024-06-07 16:40:25 -0400

    Adding some custom data to the IdentityUser by extending it with SchoolSystemUser.cs

---
+++ School System using ASPNetCore Identity/Identity.API/SchoolSystemUser.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Identity.API;
+
+/// <summary>
+/// A custom <see cref="IdentityUser"/> for the school system
+/// </summary>
+public class SchoolSystemUser : IdentityUser
+{
+    [PersonalData]
+    public string FirstName { get; set; }
+    [PersonalData]
+    public string MiddleName { get; set; }
+    [PersonalData]
+    public string LastName { get; set; }
+    [PersonalData]
+    public int Age { get; set; }
+    [PersonalData]
+    public Address CurrentAddress { get; set; }
+}
