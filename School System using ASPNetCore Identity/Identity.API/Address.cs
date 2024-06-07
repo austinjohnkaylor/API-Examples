9176e1b991f16b3d84481fc34d962879c85177e6
Austin Kaylor <AustinJKaylor@eaton.com>
2024-06-07 16:40:05 -0400

    Adding a value object for an address in the US

---
+++ School System using ASPNetCore Identity/Identity.API/Address.cs
@@ -0,0 +1,10 @@
+﻿namespace Identity.API;
+
+/// <summary>
+/// Represents an address inside the United States
+/// </summary>
+/// <param name="Street"></param>
+/// <param name="City"></param>
+/// <param name="State"></param>
+/// <param name="ZipCode"></param>
+public record Address(string Street, string City, string State, string ZipCode);
