4204f1241b6b8e8d3e122a3c1089d7d968cc85d2
Austin Kaylor <AustinJKaylor@eaton.com>
2024-08-01 18:45:38 -0400

    Adding entities for the school system

---
+++ Shared Resources/EntityFramework/SchoolSystem/Course.cs
@@ -0,0 +1,11 @@
+﻿namespace API.Examples.SharedResources.EntityFramework.SchoolSystem;
+
+public class Course
+{
+    public int CourseId { get; set; }
+    public string Title { get; set; }
+    public int Credits { get; set; }
+    public int TeacherId { get; set; }
+    public Teacher Teacher { get; set; } // Navigation property
+    public ICollection<Enrollment> Enrollments { get; set; } // Navigation property
+}
---
+++ Shared Resources/EntityFramework/SchoolSystem/Student.cs
@@ -0,0 +1,11 @@
+﻿namespace API.Examples.SharedResources.EntityFramework.SchoolSystem;
+
+public class Student
+{
+    public int StudentId { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Email { get; set; }
+    public string GradeLevel { get; set; }
+    public ICollection<Enrollment> Enrollments { get; set; } // Navigation property
+}
