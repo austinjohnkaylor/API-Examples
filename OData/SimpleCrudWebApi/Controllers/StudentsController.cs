3175ff610c4ba3a9592fdbe6171b7ffaedc34994
Austin Kaylor <AustinJKaylor@eaton.com>
2024-08-02 15:10:25 -0400

    Bootstrapping a new controller for interacting with the Student entity

---
+++ OData/SimpleCrudWebApi/Controllers/StudentsController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Examples.OData.SimpleCrudWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+    }
+}
