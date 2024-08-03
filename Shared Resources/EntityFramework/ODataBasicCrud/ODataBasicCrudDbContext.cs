88e0bd1401012dfde03dc1f1d97dd78553b09360
Austin Kaylor <AustinJKaylor@eaton.com>
2024-08-03 14:46:32 -0400

    Configuring a DbContext for the ODataBasicCrud models

---
+++ Shared Resources/EntityFramework/ODataBasicCrud/ODataBasicCrudDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace API.Examples.SharedResources.EntityFramework.ODataBasicCrud;
+
+/// <summary>
+/// 
+/// </summary>
+/// <param name="options"></param>
+/// <remarks>https://learn.microsoft.com/en-us/odata/webapi-8/tutorials/basic-crud?tabs=net60%2Cvisual-studio-2022%2Cvisual-studio%2Cvs2022#add-a-database-context</remarks>
+public class ODataBasicCrudDbContext(DbContextOptions<ODataBasicCrudDbContext> options) : DbContext(options)
+{
+    public DbSet<Customer> Customers { get; set; }
+}
