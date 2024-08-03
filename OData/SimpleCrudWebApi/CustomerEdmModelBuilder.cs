00d455ec83e935402b65b71af8638cdd305cfd04
Austin Kaylor <AustinJKaylor@eaton.com>
2024-08-03 15:30:58 -0400

    Adding an EdmModelBuilder to the SimpleCrudWebApi

---
+++ OData/SimpleCrudWebApi/CustomerEdmModelBuilder.cs
@@ -0,0 +1,16 @@
+﻿using API.Examples.SharedResources.EntityFramework.ODataBasicCrud;
+using Microsoft.OData.Edm;
+using Microsoft.OData.ModelBuilder;
+
+namespace API.Examples.OData.SimpleCrudWebApi;
+
+public static class CustomerEdmModelBuilder
+{
+    public static IEdmModel GetEdmModel()
+    {
+        ODataConventionModelBuilder builder = new();
+        builder.EnumType<CustomerType>();
+        builder.EntitySet<Customer>("Customers");
+        return builder.GetEdmModel();
+    }
+}
