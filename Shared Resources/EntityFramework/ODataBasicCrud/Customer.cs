4c60c12dbd3a4851d217567a049f2f78e49256ad
Austin Kaylor <AustinJKaylor@eaton.com>
2024-08-03 14:38:53 -0400

    Adding the Customer.cs and CustomerType entities to the Shared Resources EntityFramework project

---
+++ Shared Resources/EntityFramework/ODataBasicCrud/Customer.cs
@@ -0,0 +1,14 @@
+﻿namespace API.Examples.SharedResources.EntityFramework.ODataBasicCrud;
+
+/// <summary>
+/// 
+/// </summary>
+/// <remarks>Based off of https://learn.microsoft.com/en-us/odata/webapi-8/tutorials/basic-crud?tabs=net60%2Cvisual-studio-2022%2Cvisual-studio%2Cvs2022</remarks>
+public class Customer
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public CustomerType CustomerType { get; set; }
+    public decimal CreditLimit { get; set; }
+    public DateTime CustomerSince { get; set; }
+}
