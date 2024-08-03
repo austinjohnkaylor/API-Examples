c456535943290a4dbbe7d34abfef97431fe45dd7
Austin Kaylor <AustinJKaylor@eaton.com>
2024-08-03 15:33:00 -0400

    Adding a controller for the Customers entity

---
+++ OData/SimpleCrudWebApi/Controllers/CustomersController.cs
@@ -0,0 +1,12 @@
+﻿using API.Examples.SharedResources.EntityFramework.ODataBasicCrud;
+using Microsoft.AspNetCore.OData.Routing.Controllers;
+
+namespace API.Examples.OData.SimpleCrudWebApi.Controllers;
+
+/// <summary>
+/// An OData controller for interacting with the <see cref="Customer"/>
+/// </summary>
+public class CustomersController : ODataController
+{
+    
+}
