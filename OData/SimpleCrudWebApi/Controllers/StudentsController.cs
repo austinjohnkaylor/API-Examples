fea7ec500872771e8abb0550304525b74314d1d9
Austin Kaylor <AustinJKaylor@eaton.com>
2024-08-02 22:38:54 -0400

    Lots of things changed

--- OData/SimpleCrudWebApi/Controllers/StudentsController.cs
+++ OData/SimpleCrudWebApi/Controllers/StudentsController.cs
@@ -13,55 +13,65 @@ namespace API.Examples.OData.SimpleCrudWebApi.Controllers
     {
         // This attribute is responsible for applying the query options that are passed in the query string
         [EnableQuery]
-        public ActionResult Get()
+        public ActionResult<IQueryable<Student>> Get()
         {
             return Ok(context.Students);
         }
 
-        // This attribute is responsible for applying the query options that are passed in the query string
-        [EnableQuery]
-        public ActionResult Get([FromODataUri] int key)
+        /// <summary>
+        /// Find a <see cref="Student"/> by it's identifier.
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        public ActionResult<Student> Get([FromRoute] int key)
         {
-            return Ok(context.Students.Find(key));
+            Student? student = context.Students.Find(key);
+
+            if (student == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(student);
         }
 
-        // This attribute is responsible for applying the query options that are passed in the query string
-        [EnableQuery]
-        public ActionResult Post([FromBody] Student student)
-        {
-            context.Students.Add(student);
-            context.SaveChanges();
-            return Created(student);
-        }
-
-        // This attribute is responsible for applying the query options that are passed in the query string
-        [EnableQuery]
-        public ActionResult Put([FromODataUri] int key, [FromBody] Student student)
-        {
-            student.StudentId = key;
-            context.Students.Update(student);
-            context.SaveChanges();
-            return Updated(student);
-        }
-
-        // This attribute is responsible for applying the query options that are passed in the query string
-        [EnableQuery]
-        public ActionResult Patch([FromODataUri] int key, [FromBody] Delta<Student> student)
-        {
-            Student? entity = context.Students.Find(key);
-            student.Patch(entity);
-            context.SaveChanges();
-            return Updated(entity);
-        }
-
-        // This attribute is responsible for applying the query options that are passed in the query string
-        [EnableQuery]
-        public ActionResult Delete([FromODataUri] int key)
-        {
-            Student? entity = context.Students.Find(key);
-            context.Students.Remove(entity);
-            context.SaveChanges();
-            return NoContent();
-        }
+        // // This attribute is responsible for applying the query options that are passed in the query string
+        // [EnableQuery]
+        // public ActionResult Post([FromBody] Student student)
+        // {
+        //     context.Students.Add(student);
+        //     context.SaveChanges();
+        //     return Created(student);
+        // }
+        //
+        // // This attribute is responsible for applying the query options that are passed in the query string
+        // [EnableQuery]
+        // public ActionResult Put([FromODataUri] int key, [FromBody] Student student)
+        // {
+        //     student.StudentId = key;
+        //     context.Students.Update(student);
+        //     context.SaveChanges();
+        //     return Updated(student);
+        // }
+        //
+        // // This attribute is responsible for applying the query options that are passed in the query string
+        // [EnableQuery]
+        // public ActionResult Patch([FromODataUri] int key, [FromBody] Delta<Student> student)
+        // {
+        //     Student? entity = context.Students.Find(key);
+        //     student.Patch(entity);
+        //     context.SaveChanges();
+        //     return Updated(entity);
+        // }
+        //
+        // // This attribute is responsible for applying the query options that are passed in the query string
+        // [EnableQuery]
+        // public ActionResult Delete([FromODataUri] int key)
+        // {
+        //     Student? entity = context.Students.Find(key);
+        //     context.Students.Remove(entity);
+        //     context.SaveChanges();
+        //     return NoContent();
+        // }
     }
 }
