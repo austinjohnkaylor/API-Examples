8b7a642bb8955db2718be06687507d2df50795df
Austin Kaylor <AustinJKaylor@eaton.com>
2024-08-03 17:10:06 -0400

    Adding database options to appsettings.Development.json and a class dealing with it called DatabaseOptions.cs

---
+++ OData/SimpleCrudWebApi/Configuration/DatabaseOptions.cs
@@ -0,0 +1,7 @@
+﻿namespace API.Examples.OData.SimpleCrudWebApi.Configuration;
+
+public class DatabaseOptions
+{
+    public bool SeedData { get; set; }
+    public int CustomersToGenerate { get; set; }
+}
