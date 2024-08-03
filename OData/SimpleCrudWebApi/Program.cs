4a7f08f2d5b3590d9e0f77657ff048183ce9514f
Austin Kaylor <AustinJKaylor@eaton.com>
2024-08-03 18:35:42 -0400

    Removing an extra builder.services.addcontroller() in Program.cs

--- OData/SimpleCrudWebApi/Program.cs
+++ OData/SimpleCrudWebApi/Program.cs
@@ -19,7 +19,6 @@ builder.Services.AddControllers()
 builder.Services.AddDbContext<ODataBasicCrudDbContext>(options =>
     options.UseInMemoryDatabase("ODataBasicCrudDb"));
 
-builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
