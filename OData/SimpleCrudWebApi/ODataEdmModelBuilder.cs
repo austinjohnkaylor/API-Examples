233a25bd0076e8e46b811567fb916f60048cb8b5
Austin Kaylor <AustinJKaylor@eaton.com>
2024-08-02 22:54:35 -0400

    Creating a separate Entity Data Model in ODataEdmModelBuilder.cs for the SchoolSystem and using in Program.cs when registering the model

---
+++ OData/SimpleCrudWebApi/ODataEdmModelBuilder.cs
@@ -0,0 +1,20 @@
+﻿using API.Examples.SharedResources.EntityFramework.SchoolSystem;
+using Microsoft.OData.Edm;
+using Microsoft.OData.ModelBuilder;
+
+namespace API.Examples.OData.SimpleCrudWebApi;
+
+public static class ODataEdmModelBuilder
+{
+    public static IEdmModel GetEdmModel()
+    {
+        ODataConventionModelBuilder builder = new();
+        builder.EnumType<GradeLevel>();
+        var students = builder.EntitySet<Student>("Students");
+        students.EntityType.Ignore(student => student.Email); // Ignore the Email property --> https://learn.microsoft.com/en-us/odata/webapi/odata-security#edm-security
+        builder.EntitySet<Teacher>("Teachers");
+        builder.EntitySet<Course>("Courses");
+        builder.EntitySet<Enrollment>("Enrollments");
+        return builder.GetEdmModel();
+    }
+}
--- OData/SimpleCrudWebApi/Program.cs
+++ OData/SimpleCrudWebApi/Program.cs
@@ -1,24 +1,16 @@
+using API.Examples.OData.SimpleCrudWebApi;
 using API.Examples.SharedResources.EntityFramework.SchoolSystem;
 using Microsoft.AspNetCore.OData;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.OData.ModelBuilder;
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-ODataConventionModelBuilder modelBuilder = new();
-modelBuilder.EnumType<GradeLevel>();
-modelBuilder.EntitySet<Student>("Students");
-modelBuilder.EntitySet<Teacher>("Teachers");
-modelBuilder.EntitySet<Course>("Courses");
-modelBuilder.EntitySet<Enrollment>("Enrollments");
 
 builder.Services.AddControllers()
     .AddOData(options => options
         .EnableQueryFeatures(100)
         .AddRouteComponents(
             routePrefix: "odata",
-            model: modelBuilder.GetEdmModel()
+            model: ODataEdmModelBuilder.GetEdmModel()
             )
     );
 
@@ -46,7 +38,7 @@ app.UseAuthorization();
 app.MapControllers();
 
 // Seed database
-using (var serviceScope = app.Services.GetRequiredService<IServiceScopeFactory>().CreateScope())
+using (IServiceScope serviceScope = app.Services.GetRequiredService<IServiceScopeFactory>().CreateScope())
 {
     SchoolSystemDbContext db = serviceScope.ServiceProvider.GetRequiredService<SchoolSystemDbContext>();
     SchoolSystemDbContextHelper.SeedData(db);
