88c2fd032e91935b3912959d9791a62f0620aa6c
Austin Kaylor <AustinJKaylor@eaton.com>
2024-08-03 23:30:22 -0400

    Adding a controller for interacting with Customers

---
+++ OData/QueryOptionsApi/Controllers/CustomersController.cs
@@ -0,0 +1,32 @@
+﻿using API.Examples.OData.QueryOptionsApi.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.OData.Query;
+using Microsoft.AspNetCore.OData.Routing.Controllers;
+
+namespace API.Examples.OData.QueryOptionsApi.Controllers;
+
+public class CustomersController : ODataController
+{
+    private static readonly List<Order> Orders =
+    [
+        new Order { Id = 1001, Price = 10, Quantity = 10 },
+        new Order { Id = 1002, Price = 35, Quantity = 2 },
+        new Order { Id = 1003, Price = 70, Quantity = 5 },
+        new Order { Id = 1004, Price = 20, Quantity = 20 },
+        new Order { Id = 1005, Price = 40, Quantity = 15 },
+        new Order { Id = 1006, Price = 15, Quantity = 50 }
+    ];
+
+    private static readonly List<Customer> Customers =
+    [
+        new Customer { Id = 1, Name = "Customer 1", Age = 31, Orders = [Orders[0], Orders[1]] },
+        new Customer { Id = 2, Name = "Customer 2", Age = 32, Orders = [Orders[2], Orders[3]] },
+        new Customer { Id = 3, Name = "Customer 3", Age = 33, Orders = [Orders[4], Orders[5]] }
+    ];
+
+    [EnableQuery]
+    public IActionResult Get()
+    {
+        return Ok(Customers);
+    }
+}
