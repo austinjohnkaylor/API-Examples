56eaa7194c2e8a56ba84c34bfe6e8cc7f4bd64fe
Austin Kaylor <AustinJKaylor@eaton.com>
2024-08-03 23:18:25 -0400

    Setting up Program.cs in QueryOptionsApi to work with Odata

--- OData/QueryOptionsApi/Program.cs
+++ OData/QueryOptionsApi/Program.cs
@@ -1,13 +1,33 @@
-var builder = WebApplication.CreateBuilder(args);
+using API.Examples.OData.QueryOptionsApi.Models;
+using Microsoft.AspNetCore.OData;
+using Microsoft.OData.ModelBuilder;
+
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+ODataConventionModelBuilder modelBuilder = new();
+modelBuilder.EntitySet<Customer>("Customers");
+
+builder.Services.AddControllers().AddOData(
+    options => options
+        .Select()
+        .Filter()
+        .OrderBy()
+        .Expand()
+        .Count()
+        .SetMaxTop(null)
+        .AddRouteComponents(
+            routePrefix: "odata",
+            model: modelBuilder.GetEdmModel()
+            )
+    );
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-var app = builder.Build();
+WebApplication app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
