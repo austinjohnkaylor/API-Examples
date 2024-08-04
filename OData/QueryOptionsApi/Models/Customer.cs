14eab5b9d1f4aa85fe3f2c576b0b501bc73fd7f2
Austin Kaylor <AustinJKaylor@eaton.com>
2024-08-03 23:07:03 -0400

    Adding data models to QueryOptionsApi

---
+++ OData/QueryOptionsApi/Models/Customer.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API.Examples.OData.QueryOptionsApi.Models;
+
+public class Customer
+{
+    [Key]
+    public int Id { get; set; }
+    public String Name { get; set; }
+    public int Age { get; set; }
+    public List<Order> Orders { get; set; }
+}
---
+++ OData/QueryOptionsApi/Models/Order.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API.Examples.OData.QueryOptionsApi.Models;
+
+public class Order
+{
+    [Key]
+    public int Id { get; set; }
+    public int Price { get; set; }
+    public int Quantity { get; set; }
+}
