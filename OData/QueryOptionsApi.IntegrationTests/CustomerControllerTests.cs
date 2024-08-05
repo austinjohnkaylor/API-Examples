8a05cc68afe6a626ff2a050dd71d6d7e3624fde8
Austin Kaylor <AustinJKaylor@eaton.com>
2024-08-05 08:03:34 -0400

    Lots of changes

--- OData/QueryOptionsApi.IntegrationTests/CustomerControllerTests.cs
+++ OData/QueryOptionsApi.IntegrationTests/CustomerControllerTests.cs
@@ -1,9 +1,4 @@
-﻿using System.Net;
-using System.Text.Json.Nodes;
-using API.Examples.OData.QueryOptionsApi.Models;
-using Microsoft.AspNetCore.Mvc.Testing;
-using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
+﻿using Microsoft.AspNetCore.Mvc.Testing;
 
 namespace QueryOptionsApi.IntegrationTests;
 
@@ -22,15 +17,15 @@ public class CustomerControllerTests
     }
 
     /// <summary>
-    /// When querying the Get endpoint on the Customers controller using the $select query option with the Name property, the response should only contain the Name property of each Customer
+    /// When calling GET /odata/Customers?$select=Name, the response should contain only the Name property of each Customer
     /// </summary>
     /// <remarks>https://learn.microsoft.com/en-us/dotnet/api/microsoft.aspnetcore.odata.odataoptions.select?view=odata-aspnetcore-8.0</remarks>
     [Fact]
-    public async Task Get_SelectQueryOption_WhenQueryingByName_ShouldReturnOnlyName()
+    public async Task Get_SelectName()
     {
         // Arrange
         const string requestUri = "/odata/Customers?$select=Name";
-        string expectedResponse = await File.ReadAllTextAsync("../../../ExpectedResponses/Get_SelectQueryOption_WhenQueryingByName_ShouldReturnOnlyName.json");
+        string expectedResponse = await File.ReadAllTextAsync("../../../ExpectedResponses/Get_SelectName.json");
         
         // Act
         HttpResponseMessage response = await _httpClient.GetAsync(requestUri);
@@ -42,14 +37,33 @@ public class CustomerControllerTests
     }
 
     /// <summary>
-    /// When querying the Get endpoint on the Customers controller using the $expand query option to expand the Orders property, the response should contain the Orders property of each Customer
+    /// When calling GET /odata/Customers?$expand=Orders, the response should contain the Orders property of each Customer
     /// </summary>
     [Fact]
-    public async Task Get_ExpandQueryOption_WhenExpandingCustomersToTheirOrders_ShouldReturnCustomersAndTheirOrders()
+    public async Task Get_ExpandOrders()
     {
         // Arrange
         const string requestUri = "/odata/Customers?$expand=Orders";
-        string expectedResponse = await File.ReadAllTextAsync("../../../ExpectedResponses/Get_ExpandQueryOption_WhenExpandingCustomersToTheirOrders_ShouldReturnCustomersAndTheirOrders.json");
+        string expectedResponse = await File.ReadAllTextAsync("../../../ExpectedResponses/Get_ExpandOrders.json");
+        
+        // Act
+        HttpResponseMessage response = await _httpClient.GetAsync(requestUri);
+
+        // Assert
+        response.EnsureSuccessStatusCode();
+        string actualResponse = await response.Content.ReadAsStringAsync();
+        Assert.Equal(expectedResponse.ToFormattedJsonString(), actualResponse.ToFormattedJsonString());
+    }
+    
+    /// <summary>
+    /// When calling GET /odata/Customers?$select=Name&$expand=Orders($filter=Id gt 1004), the response should contain only the Name property of each Customer and the Orders property of each Customer should only contain Orders with an Id greater than 1004
+    /// </summary>
+    [Fact]
+    public async Task Get_SelectName_ExpandOrders_FilterIdGreaterThan1004_OrderByNameDescending()
+    {
+        // Arrange
+        const string requestUri = "odata/Customers?$select=Name&$expand=Orders($filter=Id gt 1004)&$orderby=Name desc";
+        string expectedResponse = await File.ReadAllTextAsync("../../../ExpectedResponses/Get_SelectName_ExpandOrders_FilterIdGreaterThan1004_OrderByNameDescending.json");
         
         // Act
         HttpResponseMessage response = await _httpClient.GetAsync(requestUri);
