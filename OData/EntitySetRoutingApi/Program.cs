d15c014d771db6ff5ee30d6abb1e52321ba9a23a
Austin Kaylor <AustinJKaylor@eaton.com>
2024-08-06 23:15:58 -0400

    Setting up Program.cs

--- OData/EntitySetRoutingApi/Program.cs
+++ OData/EntitySetRoutingApi/Program.cs
@@ -1,13 +1,23 @@
-var builder = WebApplication.CreateBuilder(args);
+using EntitySetRoutingApi.Models;
+using Microsoft.AspNetCore.OData;
+using Microsoft.OData.ModelBuilder;
+
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+ODataConventionModelBuilder modelBuilder = new();
+modelBuilder.EntitySet<Shape>("Shapes"); // Implicitly, Shape, Rectangle and Circle get included in the Edm model as entity types
 
-builder.Services.AddControllers();
+builder.Services.AddControllers().AddOData(
+    options => options.EnableQueryFeatures(null).AddRouteComponents(
+        routePrefix: "odata",
+        model: modelBuilder.GetEdmModel()));
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-var app = builder.Build();
+WebApplication app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -17,9 +27,11 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
+app.UseODataRouteDebug();
+app.UseRouting();
 app.UseAuthorization();
-
-app.MapControllers();
+#pragma warning disable ASP0014
+app.UseEndpoints(endpoints => endpoints.MapControllers());
+#pragma warning restore ASP0014
 
 app.Run();
