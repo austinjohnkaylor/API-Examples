a5f64bb459b2d626385970919bd76621a9a5ddcc
Austin Kaylor <AustinJKaylor@eaton.com>
2024-08-09 14:40:33 -0400

    Configuring Program.cs for use with OData

--- OData/SingletonApi/Program.cs
+++ OData/SingletonApi/Program.cs
@@ -1,6 +1,26 @@
+using API.Examples.SingletonApi.Models;
+using Microsoft.AspNetCore.OData;
+using Microsoft.OData.ModelBuilder;
+
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+ODataConventionModelBuilder modelBuilder = new();
+modelBuilder.Singleton<Company>("Company");
+
+builder.Services.AddControllers()
+    .AddOData(options =>
+    {
+        options.Select().EnableQueryFeatures(null).AddRouteComponents(
+            routePrefix: "odata",
+            model: modelBuilder.GetEdmModel());
+    });
+
 WebApplication app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseODataRouteDebug();
+app.UseRouting();
+#pragma warning disable ASP0014
+app.UseEndpoints(endpoints => endpoints.MapControllers());
+#pragma warning restore ASP0014
 
 app.Run();
