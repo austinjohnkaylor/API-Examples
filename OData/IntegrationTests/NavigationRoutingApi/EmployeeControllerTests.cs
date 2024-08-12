988c2370c076c65a40fba48ab5d4b84ad814f908
Austin Kaylor <AustinJKaylor@eaton.com>
2024-08-12 10:36:16 -0400

    Setting up the integration tests for the EmployeeController in the NavigationRoutingApi

---
+++ OData/IntegrationTests/NavigationRoutingApi/EmployeeControllerTests.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using Prog = API.Examples.OData.NavigationRoutingApi.Program;
+
+namespace API.Examples.OData.IntegrationTests.NavigationRoutingApi;
+
+/// <summary>
+/// Integration tests for the <see cref="API.Examples.OData.NavigationRoutingApi.Controllers.EmployeesController"/>
+/// </summary>
+public class EmployeeControllerTests : IClassFixture<WebApplicationFactory<Prog>>, IDisposable
+{
+    private readonly HttpClient _httpClient = new WebApplicationFactory<Prog>().CreateClient();
+
+    public void Dispose()
+    {
+        _httpClient.Dispose();
+    }
+}
