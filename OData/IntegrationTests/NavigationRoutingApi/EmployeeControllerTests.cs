dafe551df2d33316c5759c2b2e12fd2997f9cd59
Austin Kaylor <AustinJKaylor@eaton.com>
2024-08-12 11:37:16 -0400

    Adding the first test for the NavigationRoutingApi

--- OData/IntegrationTests/NavigationRoutingApi/EmployeeControllerTests.cs
+++ OData/IntegrationTests/NavigationRoutingApi/EmployeeControllerTests.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc.Testing;
+﻿using API.Examples.OData.IntegrationTests.Extensions;
+using Microsoft.AspNetCore.Mvc.Testing;
 using Prog = API.Examples.OData.NavigationRoutingApi.Program;
 
 namespace API.Examples.OData.IntegrationTests.NavigationRoutingApi;
@@ -10,8 +11,29 @@ public class EmployeeControllerTests : IClassFixture<WebApplicationFactory<Prog>
 {
     private readonly HttpClient _httpClient = new WebApplicationFactory<Prog>().CreateClient();
 
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <remarks>https://learn.microsoft.com/en-us/odata/webapi-8/fundamentals/navigation-routing?tabs=net60%2Cvisual-studio#retrieving-a-single-valued-or-collection-valued-navigation-property-on-an-entity</remarks>
+    [Fact]
+    public async Task Retrieving_a_single_valued_or_collection_valued_navigation_property_on_an_entity()
+    {
+        // Arrange
+        const string requestUri = "odata/Employees(1)/Supervisor";
+        string expectedResponse =
+            await File.ReadAllTextAsync("../../../NavigationRoutingApi/ExpectedResponses/Retrieving a single-valued or collection-valued navigation property on an entity.json");
+        // Act
+        HttpResponseMessage response = await _httpClient.GetAsync(requestUri);
+        // Assert
+        response.EnsureSuccessStatusCode();
+        string actualResponse = await response.Content.ReadAsStringAsync();
+        Assert.Equal(expectedResponse.ToFormattedJsonString(), actualResponse.ToFormattedJsonString());
+    }
+
+
     public void Dispose()
     {
+        GC.SuppressFinalize(this);
         _httpClient.Dispose();
     }
 }
