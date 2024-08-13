92c8637c807454e7eaf1fc387b716207433eee2a
Austin Kaylor <AustinJKaylor@eaton.com>
2024-08-13 11:42:43 -0400

    Configuring Program.cs in the FunctionRoutingApi so that it uses OData

--- OData/FunctionRoutingApi/Program.cs
+++ OData/FunctionRoutingApi/Program.cs
@@ -1,3 +1,7 @@
+using API.Examples.OData.FunctionRoutingApi.Models;
+using Microsoft.AspNetCore.OData;
+using Microsoft.OData.ModelBuilder;
+
 namespace API.Examples.OData.FunctionRoutingApi;
 
 public class Program
@@ -5,7 +9,31 @@ public class Program
     public static void Main(string[] args)
     {
         WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+        ODataConventionModelBuilder modelBuilder = new();
+        var employeeEntityType = modelBuilder.EntitySet<Employee>("Employees").EntityType;
+        var managerEntityType = modelBuilder.EntityType<Manager>();
+
+        employeeEntityType.Collection.Function("GetHighestRating")
+            .Returns<int>();
+        employeeEntityType.Function("GetRating")
+            .Returns<int>();
+        managerEntityType.Collection.Function("GetHighestBonus")
+            .Returns<decimal>();
+        managerEntityType.Function("GetBonus")
+            .Returns<decimal>();
+
+        builder.Services.AddControllers().AddOData(
+            options => options.EnableQueryFeatures(null).AddRouteComponents(
+                routePrefix: "odata",
+                model: modelBuilder.GetEdmModel()));
+        
         WebApplication app = builder.Build();
+        
+        app.UseODataRouteDebug();
+        app.UseRouting();
+#pragma warning disable ASP0014
+        app.UseEndpoints(endpoints => endpoints.MapControllers());
+#pragma warning restore ASP0014
         
         app.Run();
     }
